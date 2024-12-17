6bd39f1ae596653d6e9ab7f32de3d44eac9c3a79
saysaeqo <saysaeqo@gmail.com>
2024-12-18 00:02:36 +0100

    Added: output prfile file from configuration

--- src/profiler.ts
+++ src/profiler.ts
@@ -4,7 +4,13 @@ import * as path from 'path';
 import * as fs from 'fs';
 import { addInlineHints } from './decorators';
 
-const OUTPUT_FILE_NAME = "cProfile.prof";
+function getOutputFileName(): string {
+    const output = vscode.workspace.getConfiguration('python-cprofile').get<string>('cprofileOutputFileName');
+    if (!output) {
+        throw new Error('Output file name not set');
+    }
+    return output;
+}
 
 async function getPythonPath() {
     const pythonExtension = vscode.extensions.getExtension('ms-python.python');
@@ -29,7 +35,7 @@ function goToFolderWithCProfileOutput() {
     }
     let found = false;
     workspaceFolders.forEach(async (folder) => {
-        const fullPath = path.join(folder.uri.fsPath, OUTPUT_FILE_NAME);
+        const fullPath = path.join(folder.uri.fsPath, getOutputFileName());
         if (fs.existsSync(fullPath)) {
             process.chdir(folder.uri.fsPath);
             found = true;
@@ -45,7 +51,7 @@ export async function parseCProfileOutput() {
     goToFolderWithCProfileOutput();
     const pythonPath = await getPythonPath();
     const parseScriptPath = path.join(__dirname, 'parse_profile.py');
-    const parseCommand = `${pythonPath} ${parseScriptPath} ${OUTPUT_FILE_NAME}`;
+    const parseCommand = `${pythonPath} ${parseScriptPath} ${getOutputFileName()}`;
     childProcess.exec(parseCommand, (error, stdout, stderr) => {
         if (error) {
             vscode.window.showErrorMessage(`Error parsing profile data: ${stderr}`);
@@ -60,7 +66,7 @@ export async function parseCProfileOutput() {
 export async function runCProfile(filePath: string) {
     process.chdir(path.dirname(filePath));
     const pythonPath = await getPythonPath();
-    const command = `${pythonPath} -m cProfile -o ${OUTPUT_FILE_NAME} ${filePath}`;
+    const command = `${pythonPath} -m cProfile -o ${getOutputFileName()} ${filePath}`;
     childProcess.exec(command, (error, stdout, stderr) => {
         if (error) {
             vscode.window.showErrorMessage(`Error profiling file: ${stderr}`);
